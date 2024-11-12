5ecf39d715a28aba066fbcc3828576f002239124
Felipe Lustosa <lustosaf590@gmail.com>
2024-11-12 14:57:14 -0300

    controllers insert, update and delete

--- src/main/java/com/devsuperior/crud/client/controllers/ClientController.java
+++ src/main/java/com/devsuperior/crud/client/controllers/ClientController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -21,7 +21,7 @@ public class ClientController {
     @GetMapping(value ="/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = services.findById(id);
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(dto);//200 ok
     }
 
     @GetMapping
@@ -30,6 +30,25 @@ public class ClientController {
         return  ResponseEntity.ok(all);
     }
 
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto = services.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);//201 Created
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = services.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        services.delete(id);
+        return ResponseEntity.noContent().build();//204  indica que o servidor processou com sucesso a solicitação, mas que não há conteúdo a ser enviado de volta no corpo da resposta.
+    }
 
     
 }
